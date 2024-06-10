9a973d02b29e35ac80acdcb4e8c681e6df1a1f74
Sebastian Puck <info@pucks.me>
2024-06-10 18:22:49 +0200

    [SM/S-2] add server name, address and port to translations

--- zeus-compiler/src/main/java/zeus/zeuscompiler/rain/compiler/syntaxtree/Server.java
+++ zeus-compiler/src/main/java/zeus/zeuscompiler/rain/compiler/syntaxtree/Server.java
@@ -3,6 +3,7 @@ package zeus.zeuscompiler.rain.compiler.syntaxtree;
 import zeus.zeuscompiler.CompilerError;
 import zeus.zeuscompiler.rain.dtos.ExportTarget;
 import zeus.zeuscompiler.thunder.compiler.symboltable.SymbolTable;
+import zeus.zeuscompiler.utils.CompilerUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,9 +22,23 @@ public class Server extends Node {
 
     @Override
     public String translate(SymbolTable symbolTable, int depth, ExportTarget exportTarget) {
-        return this.routes.stream()
-          .map(route -> route.translate(symbolTable, depth, exportTarget))
-          .collect(Collectors.joining("\n"));
+        return switch (exportTarget) {
+            case REACT_TYPESCRIPT -> String.format(
+              CompilerUtils.buildLinesFormat(new String[]{
+                "import {app} from './index.ts';",
+                "export const name = \"%s\";",
+                "export const address = \"%s\";",
+                "export const port = \"%s\";",
+                "%s"
+              }, depth),
+              this.name,
+              this.address,
+              this.port,
+              this.routes.stream()
+                .map(route -> route.translate(symbolTable, depth, exportTarget))
+                .collect(Collectors.joining("\n"))
+            );
+        };
     }
 
     @Override
