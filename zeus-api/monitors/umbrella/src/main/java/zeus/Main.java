a09373169a82c5124de49626f9ab11101bfdf47b
Sebastian Puck <info@pucks.me>
2024-10-21 18:41:38 +0200

    [SM/M-2] implement multithreaded request handling

--- zeus-api/monitors/umbrella/src/main/java/zeus/Main.java
+++ zeus-api/monitors/umbrella/src/main/java/zeus/Main.java
@@ -3,20 +3,46 @@ package zeus;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     System.out.println("On-runtime, temporal, formal verification monitor");
+
     ServerSocket serverSocket = new ServerSocket(8081);
-    Socket socket = serverSocket.accept();
-    Request request = new Request(socket.getInputStream());
+    ExecutorService executorService = Executors.newCachedThreadPool();
 
-    if (!request.isValid()) {
-      socket.getOutputStream().write(new Response(400, "{\"error\": \"invalid request\"}").toBytes());
-      return;
+    while (true) {
+      Socket socket = serverSocket.accept();
+      executorService.submit(() -> {
+        try {
+          Request request = new Request(socket.getInputStream());
+
+          if (!request.isValid()) {
+            socket.getOutputStream().write(new Response(
+              400,
+              "{\"error\": \"invalid request\"}"
+            ).toBytes());
+            socket.close();
+            return;
+          }
+
+          socket.getOutputStream().write(new Response(
+            200,
+            "{\"reponseData\": \"test\"}"
+          ).toBytes());
+          socket.close();
+        } catch (IOException ioException) {
+          ioException.printStackTrace();
+        } finally {
+          try {
+            socket.close();
+          } catch (IOException ioException) {
+            ioException.printStackTrace();
+          }
+        }
+      });
     }
-
-    socket.getOutputStream().write(new Response(200, "{\"reponseData\": \"test\"}").toBytes());
-    socket.close();
   }
 }
