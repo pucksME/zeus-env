4cda8bcc8e00e46edb975d37b1a0390a8269cdb5
Sebastian Puck <info@pucks.me>
2026-05-12 08:01:32 +0200

    add experiment run scripts

---
+++ zeus-verifier/experiments/constraint-extraction/run.sh
@@ -0,0 +1,4 @@
+python3 ../../../utils/verify_code_module.py \
+  --address localhost:3333 \
+  --input-file constraint-extraction-1.rain \
+  --code-module-name Extract
---
+++ zeus-verifier/experiments/test-generation/run.sh
@@ -0,0 +1,4 @@
+python3 ../../../utils/verify_code_module.py \
+  --address localhost:3333 \
+  --input-file test-generation-1.rain \
+  --code-module-name Generate
